a6de1ba563129231d21cfcd1bac68e2c255b3805
panutat-p <pp.panutat@gmail.com>
2022-06-30 02:19:24 +0700

    connect DB, define User schema, run auto migration

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # simple-todo-gin
 
-
 ```shell
 go mod init github.com/panutat-p/simeple-todo-gin
-go install github.com/gin-gonic/gin@latest
+
+go get -u github.com/gin-gonic/gin
+go get -u gorm.io/gorm
+go get -u gorm.io/driver/postgres
+go get -u github.com/joho/godotenv
 ```
--- main.go
+++ main.go
@@ -3,19 +3,48 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
+	"os"
 )
 
+type User struct {
+	gorm.Model
+	Name string
+}
+
 func main() {
-	fmt.Println("Hello")
+	err := godotenv.Load()
+	if err != nil {
+		panic("Failed to load ENV")
+	}
+	dsn := os.Getenv("DATABASE_DSN")
+	fmt.Println("dsn:", dsn)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect ElephantSQL")
+	}
+	fmt.Println("DB connection is established")
+
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		panic("Failed to migrate ElephantSQL")
+	}
+
+	db.Create(&User{Name: "Monkey"})
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	err := r.Run()
+
+	err = r.Run()
 	if err != nil {
 		log.Println("server crashed")
 		return
